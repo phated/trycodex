f67da91888d11a8fb921593d3e63cba354b54e54
Blaine Bublitz <phate666x@gmail.com>
2012-01-06 23:00:36 -0700

    cp codex work

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (trycodex) is running..');
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+ 
+    //console.log('request starting...');
+     
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './fluid.html';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
 }).listen(13465);
